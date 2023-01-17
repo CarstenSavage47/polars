2bede74aa6735dae65a5887721c1b0103a27bd4c
CarstenSavage47 <rampagev2@gmail.com>
2023-01-17 07:26:33 -0800

    Added function

--- main.py
+++ main.py
@@ -10,10 +10,16 @@ iris.columns
 
 q = (
     iris
+    # The asteriks stands for all columns.
+    .select(polars.col('*'))
     .filter(polars.col("SepalLength") > 5)
     .groupby("Species")
     .agg(polars.all().sum())
 )
+
+q.head(5)
+
+iris.describe()
 
 q.write_csv("/Users/carstenjuliansavage/Documents/q.csv")
 
