e353b3d2eed32454859b2f6b3f161d70aeb47a92
Carsten <rampagev2@gmail.com>
2023-04-27 16:16:41 -0700

    Added function

--- main.py
+++ main.py
@@ -4,22 +4,39 @@ import numpy as np
 import pandas
 import pyarrow
 
-iris = polars.read_csv("/Users/carstenjuliansavage/Desktop/R Working Directory/Useful Datasets/iris.csv")
+iris = polars.read_csv(
+    "/Users/carstenjuliansavage/Desktop/R Working Directory/Useful Datasets/iris.csv"
+)
 
 iris.columns
 
-q = (
+iris_all_sums = (
     iris
     # The asteriks stands for all columns.
-    .select(polars.col('*'))
+    .select(polars.col("*"))
     .filter(polars.col("SepalLength") > 5)
     .groupby("Species")
     .agg(polars.all().sum())
 )
 
-q.head(5)
+iris_transformed = iris.select(
+    [
+        polars.col("*"),
+        polars.col("SepalLength").sort().alias("Sorted_SepalLength"),
+        polars.col("SepalWidth")
+        .filter(polars.col("SepalWidth") > 0)
+        .sum()
+        .alias("Sum_SepalWidth"),
+    ]
+).drop(["SepalWidth", "SepalLength"])
 
-iris.describe()
+iris_transformed_pd = iris_transformed.to_pandas()
 
-q.write_csv("/Users/carstenjuliansavage/Documents/q.csv")
+ndistinct_counts = iris.select(
+    [
+        polars.col("SepalLength").n_unique().alias("SepalLength_ndistinct"),
+        polars.col("SepalWidth").unique().count().alias("SepalWidth_ndistinct"),
+    ]
+)
+print(ndistinct_counts)
 
